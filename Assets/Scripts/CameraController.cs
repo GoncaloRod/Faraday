f7e462eba808ea4c7f6124344c3681b949edae19
Gonçalo Rodrigues <rodriguesgonalo604@gmail.com>
2020-07-03 19:49:14 +0100

    Camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private InputActions _inputActions;
+
+    private Vector3 _newPosition;
+    private Quaternion _newRotation;
+    
+    private Vector3 _currentZoom;
+    private Vector3 _newZoom;
+
+    [SerializeField] private Transform cameraTransform = null;
+    [SerializeField] private Transform cameraTarget = null;
+
+    [SerializeField] private float smoothFactor = 1f;
+
+    [SerializeField] private float movementSpeed = 1f;
+    [SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private float zoomSpeed = 1f;
+
+
+    private void Awake()
     {
-        
+        _inputActions = new InputActions();
+
+        _newPosition = transform.position;
+        _newRotation = transform.rotation;
+
+        _currentZoom = _newZoom = cameraTransform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // Get inputs
+        Vector2 movementInput = _inputActions.CameraControls.Movement.ReadValue<Vector2>();
+        float rotationInput = _inputActions.CameraControls.Rotation.ReadValue<float>();
+        float zoomInput = _inputActions.CameraControls.Zoom.ReadValue<float>();
         
+        // Calculate new positions
+        _newPosition += (transform.right * movementInput.x + transform.forward * movementInput.y) * (movementSpeed * Time.deltaTime);
+        _newRotation *= Quaternion.Euler(Vector3.up * (rotationInput * rotationSpeed * Time.deltaTime));
+        _newZoom += new Vector3(0, -1, 1) * (zoomInput * zoomSpeed * Time.deltaTime);
+
+        if (_newZoom.y >= cameraTarget.position.y + 1f)
+            _currentZoom = _newZoom;
+        else
+            _newZoom = _currentZoom;
+
+        // Apply position with interpolation
+        transform.position = Vector3.Lerp(transform.position, _newPosition, Time.deltaTime * smoothFactor);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _newRotation, Time.deltaTime * smoothFactor);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, _currentZoom, Time.deltaTime * smoothFactor);
+
+        cameraTransform.LookAt(cameraTarget);
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
     }
 }
---
+++ Assets/Scripts/InputActions.cs
@@ -0,0 +1,311 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Camera Controls"",
+            ""id"": ""3ffa3361-000e-4e0b-a13a-b1db9ae50f49"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""41ebb911-c480-4894-9ddc-7fea843b3d84"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Rotation"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""6a6c0e28-a864-422a-abe4-f74ec3935710"",
+                    ""expectedControlType"": ""Analog"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Zoom"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""9264f75c-d6b7-4039-a988-c5cd6fd49ca5"",
+                    ""expectedControlType"": ""Analog"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD Keys"",
+                    ""id"": ""411916d7-b9d8-4a9b-92d9-1b2ad1c956bc"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""5614ddc9-801e-439c-9f41-400f8d585abc"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""6bcc3e00-d455-4882-b972-35ca5c27c77c"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""438e7da5-997f-4b95-8f9c-1360aaae2ef2"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""f0dc6d4f-41f2-4c1a-b174-da0bd36cc949"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Arrow Keys"",
+                    ""id"": ""2ab8e2d9-fd4f-4f58-b473-6ef1465b6645"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""d03c245f-ceaf-42de-942c-6d4d4a8e78ab"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""8b0f596c-982a-4108-be4f-6047a9a33372"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""0362b6d5-b76b-4251-b0c7-49fdfbf5e95d"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""a4686d3b-47aa-42d7-953f-34f34dff5cf9"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""QE Keys"",
+                    ""id"": ""fa837f43-aeed-4471-9b19-bb1b2a292bd7"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotation"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""721fd9c9-56aa-4c14-9347-6020e4d3264f"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""8e432a93-8b52-46a6-a2e4-13e8d596776e"",
+                    ""path"": ""<Keyboard>/q"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""70976c8c-eac6-4ca0-bfbe-ca0bcbe01102"",
+                    ""path"": ""<Mouse>/scroll/y"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Zoom"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Camera Controls
+        m_CameraControls = asset.FindActionMap("Camera Controls", throwIfNotFound: true);
+        m_CameraControls_Movement = m_CameraControls.FindAction("Movement", throwIfNotFound: true);
+        m_CameraControls_Rotation = m_CameraControls.FindAction("Rotation", throwIfNotFound: true);
+        m_CameraControls_Zoom = m_CameraControls.FindAction("Zoom", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Camera Controls
+    private readonly InputActionMap m_CameraControls;
+    private ICameraControlsActions m_CameraControlsActionsCallbackInterface;
+    private readonly InputAction m_CameraControls_Movement;
+    private readonly InputAction m_CameraControls_Rotation;
+    private readonly InputAction m_CameraControls_Zoom;
+    public struct CameraControlsActions
+    {
+        private @InputActions m_Wrapper;
+        public CameraControlsActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_CameraControls_Movement;
+        public InputAction @Rotation => m_Wrapper.m_CameraControls_Rotation;
+        public InputAction @Zoom => m_Wrapper.m_CameraControls_Zoom;
+        public InputActionMap Get() { return m_Wrapper.m_CameraControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CameraControlsActions set) { return set.Get(); }
+        public void SetCallbacks(ICameraControlsActions instance)
+        {
+            if (m_Wrapper.m_CameraControlsActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnMovement;
+                @Rotation.started -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnRotation;
+                @Rotation.performed -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnRotation;
+                @Rotation.canceled -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnRotation;
+                @Zoom.started -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnZoom;
+                @Zoom.performed -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnZoom;
+                @Zoom.canceled -= m_Wrapper.m_CameraControlsActionsCallbackInterface.OnZoom;
+            }
+            m_Wrapper.m_CameraControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Rotation.started += instance.OnRotation;
+                @Rotation.performed += instance.OnRotation;
+                @Rotation.canceled += instance.OnRotation;
+                @Zoom.started += instance.OnZoom;
+                @Zoom.performed += instance.OnZoom;
+                @Zoom.canceled += instance.OnZoom;
+            }
+        }
+    }
+    public CameraControlsActions @CameraControls => new CameraControlsActions(this);
+    public interface ICameraControlsActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnRotation(InputAction.CallbackContext context);
+        void OnZoom(InputAction.CallbackContext context);
+    }
+}
