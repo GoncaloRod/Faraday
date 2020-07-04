727a12c6bcd0e3bd017e2c8d88188ea380c215af
Carlos Castanheira <aereongames@gmail.com>
2020-07-04 18:48:08 +0100

    Added a battery controller to set the charge value

---
+++ Assets/Scripts/UI/BatteryController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryController : MonoBehaviour
+{
+    #region Singleton
+
+    private static BatteryController _instance;
+
+    public static BatteryController Instance => _instance;
+
+    #endregion
+
+    public GameObject BarsObject;
+    private GameObject[] _bars;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this) Destroy(gameObject);
+        else _instance = this;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        int size = BarsObject.transform.childCount;
+        _bars = new GameObject[size];
+
+        for (int i = 0; i < size; ++i)
+        {
+            _bars[i] = BarsObject.transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // Value goes from 1 to 9
+    public void SetCharge(int value)
+    {
+        for (int i = 0; i < _bars.Length; ++i)
+        {
+            GameObject gb = _bars[i];
+            // Value - 1 it's converts the first bar of the battery to the first array element
+            gb.SetActive(value - 1 >= i);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject shopBar;
+    public GameObject battery;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleShopBar()
+    {
+        float shopBarHeight = shopBar.GetComponent<RectTransform>().rect.height / 1.65f ;
+
+        RectTransform battRectTransform = battery.GetComponent<RectTransform>();
+        if (!shopBar.activeSelf)
+        {
+            Debug.Log($"BattY: {battRectTransform.position.y} | ShopBarHeight: {shopBarHeight}");
+            battRectTransform.position = new Vector3(battRectTransform.position.x, battRectTransform.position.y + shopBarHeight);
+            Debug.Log($"BattY: {battRectTransform.position.y} | ShopBarHeight: {shopBarHeight}");
+            shopBar.SetActive(true);
+        }
+        else
+        {
+            battRectTransform.position = new Vector3(battRectTransform.position.x, battRectTransform.position.y - shopBarHeight);
+            shopBar.SetActive(false);
+        }
+    }
+
+    private int charge = 0;
+    public void GoBatteryUp()
+    {
+        BatteryController.Instance.SetCharge(++charge);
+    }
+
+    public void GoBatteryDown()
+    {
+        BatteryController.Instance.SetCharge(--charge);
+    }
+
+}
