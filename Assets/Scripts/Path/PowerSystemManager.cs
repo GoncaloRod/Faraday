edc321637509420c414f46d746d411d198a008f6
NockPT <araujonuno2000@gmail.com>
2020-07-04 20:02:46 +0100

    Power System

--- Assets/Scripts/Path/PowerSystemManager.cs
+++ Assets/Scripts/Path/PowerSystemManager.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class PowerSystemManager : MonoBehaviour
 {
-    public GameObject[] Batteries;
+
+
+    private static PowerSystemManager _instance;
+    public static PowerSystemManager Instance => _instance;   
+
     public float powerMade;
     public float powerUsed = 0;
     public float powerAvailable;
+    public int maxSolarPanels = 0;
+    public int maxEnergyCap = 0;
+
+    public List<SolarPanel> SolarPanelsList = new List<SolarPanel>();
+    public List<Battery> BatteriesList = new List<Battery>();
+    public List<ElectricalBox> ElectricBoxesList = new List<ElectricalBox>();
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    private void Awake(){
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        powerMade = TotalPower();
+    void Update(){
         powerAvailable = powerMade + powerUsed;
     }
 
-    public float TotalPower()
-    {       
-        float total = 0;
-        for (int c = 0; c < Batteries.Length; c++ ) { 
-            total +=  (float)System.Math.Round(Batteries[c].GetComponent<BatteryManager>().energyStored, 2);
-        }
-        return total;
+    public void addEletricBox(){
+        
+    }
+
+    public void addSolarPanel(){
+
+    }
+
+    public void addBattery(){
+        
     }
 }
