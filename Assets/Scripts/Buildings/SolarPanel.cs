66fd0fa3c04284e1ec33ca2beae1d7ec39afa319
Gonçalo Rodrigues <rodriguesgonalo604@gmail.com>
2020-07-05 15:35:27 +0100

    yeet

--- Assets/Scripts/Buildings/SolarPanel.cs
+++ Assets/Scripts/Buildings/SolarPanel.cs
@@ -9,18 +9,10 @@ public class SolarPanel : Building
     [Header("Solar Panel Settings")]
     [SerializeField] private float optimumPowerOutput = 100;
 
-    [Header("Dirtiness Settings")]
-    [SerializeField] private float timeToDirty = 10 * 60;
-    [SerializeField] private float cleaningDownTime = 1 * 60;
-
     public float CurrentOutput { get; private set; }
 
     private void Update()
     {
-        _lastClear += Time.deltaTime;
-
-        float dirtyPercentage = Mathf.Clamp(_lastClear / timeToDirty, 0f, 1f);
-
-        CurrentOutput = optimumPowerOutput * (1 - dirtyPercentage);
+        CurrentOutput = optimumPowerOutput;
     }
 }
